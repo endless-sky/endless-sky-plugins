7eb5e94d528a3ffc7ef7d69212b0719377ce361b
M*C*O <mcofficer@gmx.de>
2020-06-28 13:16:08 +0200

    Adjust check_urls.py to also check download links

--- scripts/check_urls.py
+++ scripts/check_urls.py
@@ -1,30 +1,45 @@
 #!/usr/bin/python3
 
-# Checks all URLs it can find in plugins.yaml
+# Checks constructed download URLs and all other URLs it can find
 # Usage: ./check_urls.py plugins.yaml
 
 import yaml
 import sys
 from urllib.parse import urlparse
 from urllib.error import HTTPError
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+
+
+def test(url):
+    print("Testing " + url)
+    try:
+        conn = urlopen(Request(url, method="HEAD"))
+        return True
+    except HTTPError as e:
+        print("%s: %s" % (e.code, e.reason))
+        return False
+
 
 file = sys.argv[1]
 with open(file, "r") as f:
     plugins = yaml.load(f, Loader=yaml.FullLoader)
 
+
 valid = True
 for plugin in plugins:
+
+    # Test download link
+    download_link = "%s/archive/%s.zip" % (plugin["url"], plugin["version"])
+    if not test(download_link):
+        valid = False
+
+    # Find and test any other links
     for value in plugin.values():
         url = urlparse(value)
         # urlparse will happily parse any string as relative URL,
-        # but only those with `netloc` are absolute, valid URLs.
+        # but only those with `netloc` are absolute, VALID URLs.
         if url.netloc:
-            print("Testing " + value)
-            try:
-                conn = urlopen(value)
-            except HTTPError as e:
-                print("%s: %s" % (e.code, e.reason))
+            if not test(value):
                 valid = False
 
 if not valid:
